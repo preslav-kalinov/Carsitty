6f9f651ac03effcb4159f6f8538939a8d84ea24e
preslav-kalinov <preslav_00_k@abv.bg>
2023-03-13 01:09:56 +0200

    Created Spring application

---
+++ src/main/java/com/kalinov/carsitty/CarsittyApplication.java
@@ -0,0 +1,13 @@
+package com.kalinov.carsitty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsittyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsittyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kalinov/carsitty/CarsittyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalinov.carsitty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsittyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
