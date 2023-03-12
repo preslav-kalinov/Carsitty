5811bccb34d697f2db3047a07b11686538f2d4b1
preslav-kalinov <preslav_00_k@abv.bg>
2023-03-13 01:23:22 +0200

    Create database sql file

---
+++ sql/carsitty.sql
@@ -0,0 +1,91 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+CREATE DATABASE IF NOT EXISTS `carsitty` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `carsitty`;
+
+CREATE TABLE `cars` (
+  `id` bigint(20) NOT NULL,
+  `brand` varchar(128) NOT NULL,
+  `model` varchar(1024) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `categories` (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `logs` (
+  `id` bigint(20) NOT NULL,
+  `incidentTime` datetime NOT NULL DEFAULT current_timestamp(),
+  `errorMessage` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `parts` (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(1024) CHARACTER SET utf8 NOT NULL,
+  `quantity` bigint(20) UNSIGNED NOT NULL,
+  `price` decimal(10,2) UNSIGNED NOT NULL DEFAULT 0.00,
+  `categoryId` bigint(20) NOT NULL,
+  `carId` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `users` (
+  `id` bigint(20) NOT NULL,
+  `username` varchar(128) NOT NULL,
+  `displayName` varchar(1024) NOT NULL,
+  `password` varchar(1024) NOT NULL,
+  `email` varchar(1024) NOT NULL,
+  `role` enum('Employee','Manager','Administrator') NOT NULL DEFAULT 'Employee' COMMENT '''1 - Employee(default),\\r\\n2 - Manager,\\r\\n3 - Administrator''',
+  `enabled` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `cars`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `brand` (`brand`,`model`) USING HASH;
+
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+ALTER TABLE `logs`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `parts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `categoryId` (`categoryId`),
+  ADD KEY `carId` (`carId`);
+
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+ALTER TABLE `cars`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `categories`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `logs`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `parts`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `parts`
+  ADD CONSTRAINT `parts_ibfk_1` FOREIGN KEY (`carId`) REFERENCES `cars` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `parts_ibfk_2` FOREIGN KEY (`categoryId`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
