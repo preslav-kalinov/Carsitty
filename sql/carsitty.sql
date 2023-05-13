08c0155e4ebb10824afb28a31ff9c93ac8febc49
preslav-kalinov <preslav_00_k@abv.bg>
2023-05-13 17:32:20 +0300

    Updated database sql file

--- sql/carsitty.sql
+++ sql/carsitty.sql
@@ -2,18 +2,16 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
-CREATE DATABASE IF NOT EXISTS `carsitty` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `carsitty`;
-
 CREATE TABLE `cars` (
   `id` bigint(20) NOT NULL,
   `brand` varchar(128) NOT NULL,
-  `model` varchar(1024) NOT NULL
+  `modelId` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `categories` (
@@ -27,13 +25,24 @@ CREATE TABLE `logs` (
   `errorMessage` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+CREATE TABLE `models` (
+  `id` bigint(20) NOT NULL,
+  `model` varchar(1024) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 CREATE TABLE `parts` (
   `id` bigint(20) NOT NULL,
   `name` varchar(1024) CHARACTER SET utf8 NOT NULL,
   `quantity` bigint(20) UNSIGNED NOT NULL,
   `price` decimal(10,2) UNSIGNED NOT NULL DEFAULT 0.00,
   `categoryId` bigint(20) NOT NULL,
-  `carId` bigint(20) NOT NULL
+  `carId` bigint(20) NOT NULL,
+  `userId` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `roles` (
+  `id` bigint(20) NOT NULL,
+  `role` enum('Employee','Manager','Administrator','') NOT NULL DEFAULT 'Employee' COMMENT '1 - Employee(default),\r\n2 - Manager,\r\n3 - Administrator'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `users` (
@@ -42,13 +51,14 @@ CREATE TABLE `users` (
   `displayName` varchar(1024) NOT NULL,
   `password` varchar(1024) NOT NULL,
   `email` varchar(1024) NOT NULL,
-  `role` enum('Employee','Manager','Administrator') NOT NULL DEFAULT 'Employee' COMMENT '''1 - Employee(default),\\r\\n2 - Manager,\\r\\n3 - Administrator''',
+  `roleId` bigint(20) NOT NULL,
   `enabled` tinyint(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 ALTER TABLE `cars`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `brand` (`brand`,`model`) USING HASH;
+  ADD UNIQUE KEY `brand` (`brand`),
+  ADD KEY `cars_ibfk_1` (`modelId`);
 
 ALTER TABLE `categories`
   ADD PRIMARY KEY (`id`),
@@ -57,14 +67,24 @@ ALTER TABLE `categories`
 ALTER TABLE `logs`
   ADD PRIMARY KEY (`id`);
 
+ALTER TABLE `models`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `model` (`model`) USING HASH;
+
 ALTER TABLE `parts`
   ADD PRIMARY KEY (`id`),
   ADD KEY `categoryId` (`categoryId`),
-  ADD KEY `carId` (`carId`);
+  ADD KEY `carId` (`carId`),
+  ADD KEY `userId` (`userId`) USING BTREE;
+
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
 
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`);
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`) USING HASH,
+  ADD KEY `users_ibfk_1` (`roleId`);
 
 ALTER TABLE `cars`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
@@ -75,15 +95,28 @@ ALTER TABLE `categories`
 ALTER TABLE `logs`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 
+ALTER TABLE `models`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
 ALTER TABLE `parts`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `roles`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `users`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 
+ALTER TABLE `cars`
+  ADD CONSTRAINT `cars_ibfk_1` FOREIGN KEY (`modelId`) REFERENCES `models` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
 ALTER TABLE `parts`
   ADD CONSTRAINT `parts_ibfk_1` FOREIGN KEY (`carId`) REFERENCES `cars` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `parts_ibfk_2` FOREIGN KEY (`categoryId`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `parts_ibfk_2` FOREIGN KEY (`categoryId`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `parts_ibfk_3` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`roleId`) REFERENCES `roles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
