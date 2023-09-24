6596764d9ec2d6596789de19eeb09671a9f7336b
preslav-kalinov <preslav_00_k@abv.bg>
2023-09-24 14:28:45 +0300

    [Backend] Make email sending asynchroneous

--- src/backend/src/main/java/com/kalinov/carsitty/service/MailService.java
+++ src/backend/src/main/java/com/kalinov/carsitty/service/MailService.java
@@ -3,6 +3,8 @@ package com.kalinov.carsitty.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Service;
 
 import javax.mail.Message;
@@ -11,6 +13,7 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.List;
 
+@EnableAsync
 @Service
 public class MailService {
     private final JavaMailSender javaMailSender;
@@ -26,6 +29,7 @@ public class MailService {
         this.javaMailSender = javaMailSender;
     }
 
+    @Async
     public void sendEmail(List<String> recipients, String subject, String text) throws MessagingException {
         MimeMessage message = this.javaMailSender.createMimeMessage();
         message.setFrom(String.format("%s <%s>", this.companyName, this.senderEmail));
